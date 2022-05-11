e17109b46a3d22a0026b4655af937d6ca5c38344
goncalon <goncalosilvan@gmail.com>
2022-05-11 22:23:21 +0100

    AJAX e Form para criar e listar Medicamentos
    Quando a tabela não tenha imagens, o AJAX não pode ser por FormData, pois neste caso estamos só a passar Strings, e não ficheiros.
    Pequena correção no controlador, pois estava com virgula mais

--- frontendApoioDomicilio/www/js/CriaMed.js
+++ frontendApoioDomicilio/www/js/CriaMed.js
@@ -6,14 +6,16 @@ var app = {
 onDeviceReady: function(){
     $("#formulario").submit(function (event){
        event.preventDefault();
-       var formData = new FormData(this);
        $.ajax({
            type: "POST",
            url: "http://localhost:8080/api/medicamentos/addMed",
-           data: formData,
-           cache: false,
-           processData: false, 
-           contentType: false, 
+           data: {
+               nome: $("#nome").val(),
+               farmaceutica: $("#farmaceutica").val(),
+               principio: $("#principio").val(),
+               dosagem: $("#dosagem").val(),
+               formato: $("#formato").val(),
+        },
            sucess: function( data ){
                alert(data);
            }
--- frontendApoioDomicilio/www/js/listaMed.js
+++ frontendApoioDomicilio/www/js/listaMed.js
@@ -1,5 +1,5 @@
 
-    $("#usersTable").ready(function (data){
+    $("#medTable").ready(function (data){
 
         $.get( "http://localhost:8080/api/medicamentos/allMed", function( data ) {
             for (var i = 0; i < data.length; i++) {
@@ -9,11 +9,11 @@
                             <td>${data[i].principio}</td>
                             <td>${data[i].dosagem}</td>
                             <td>${data[i].formato}</td>
-                            // <td><a href="/detalheMed.html" onclick="location.href=this.href+'?id='+${data[i].id};return false;">Detalhe</a></td>
+                            <td><a href="/detalheMed.html" onclick="location.href=this.href+'?id='+${data[i].id};return false;">Detalhe</a></td>
                             <td><a href="/editaMed.html" onclick="location.href=this.href+'?id='+${data[i].id};return false;">Editar</a></td>
                             <td><a href="" onclick="remover(${data[i].id})">Remover</a></td>
                            </tr>`
-                $("#usersTable tbody").append(row)
+                $("#medTable tbody").append(row)
               }
           });
        
